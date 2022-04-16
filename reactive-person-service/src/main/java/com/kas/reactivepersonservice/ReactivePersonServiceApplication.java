296fcaa4d2c9455d3ce131dc693847a4da7bc5d1
KasNotten <kasnotten1@hotmail.com>
2022-04-16 22:34:56 +0200

    backend initial

---
+++ reactive-person-service/src/main/java/com/kas/reactivepersonservice/ReactivePersonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kas.reactivepersonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivePersonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivePersonServiceApplication.class, args);
+	}
+
+}
---
+++ reactive-person-service/src/test/java/com/kas/reactivepersonservice/ReactivePersonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kas.reactivepersonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivePersonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
